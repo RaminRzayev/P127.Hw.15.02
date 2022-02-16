eda722d76ce75effa937c7af9513627c652e62b2
RaminRzayev <raminag@code.edu.az>
2022-02-16 04:28:56 +0400

    Add task

---
+++ P127.HW.15.02/P127.HW.15.02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P127.HW._15._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Task = "Confrontation Alley of Fame";
+            Console.WriteLine(ChangeLetter(Task));
+        }
+        public static string ChangeLetter(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+               
+                if (str[i]==char.ToUpper(str[i]))
+                {
+                    char.ToLower(str[i]);
+                }
+                else
+                {
+                    char.ToUpper(str[i]);
+                }
+                
+            }
+            return str;
+        }
+    }
+}
